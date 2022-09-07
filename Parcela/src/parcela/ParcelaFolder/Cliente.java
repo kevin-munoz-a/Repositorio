8fcfec166429292a88ca09218ff5efd341bda72c
Kevin Muñoz A <107315660+kevin-munoz-a@users.noreply.github.com>
2022-09-06 22:04:50 -0300

    Update Cliente.java

--- Parcela/src/parcela/ParcelaFolder/Cliente.java
+++ Parcela/src/parcela/ParcelaFolder/Cliente.java
@@ -1,75 +1,83 @@
-
 package parcela.ParcelaFolder;
 
 public class Cliente {
-    public int Run;
-    public String Dv;
-    public String Nombre;
-    public String Email;
-    public String Direccion;
-    public int Telefono;
-    
+    private int Run;
+    private String Dv;
+    private String Nombre;
+    private String Mail;
+    private String Direccion;
+    private int Telefono;
+
     public Cliente() {
         this.Run=0;
         this.Dv="";
         this.Nombre="";
-        this.Email="";
+        this.Mail="";
         this.Direccion="";
         this.Telefono=0;
     }
-    
-    
-    
-    
+
+    public Cliente(int Run, String Dv, String Nombre, String Mail, String Direccion, int Telefono) {
+        this.Run = Run;
+        this.Dv = Dv;
+        this.Nombre = Nombre;
+        this.Mail = Mail;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+    }
+
+
+
+
     public int getRun() {
         return this.Run;
     }
     public void setRun(int Run) {
         this.Run = Run;
     }
-    
-    
-    
-    
+
+
+
+
     public String getDv() {
         return this.Dv;
     }
     public void setDv(String Dv){
         this.Dv = Dv;
     }
-    
-    
-    
-    
+
+
+
+
     public String getNombre() {
         return this.Nombre;
     }
     public void setNombre(String Nombre){
         this.Nombre = Nombre;
     }
-    
-    
-    
-    
+
+
+
+
     public String getEmail() {
-        return this.Email;
+        return this.Mail;
     }
     public void setEmail(String Email) {
-        this.Email = Email;
+        this.Mail = Email;
     }
-    
-        
-    
-    
+
+
+
+
     public String getDireccion() {
         return this.Direccion;
     }
     public void setDireccion (String Direccion) {
         this.Direccion = Direccion;
     }
-    
-    
-    
+
+
+
     public int getTelefono() {
         return this.Telefono;
     }
